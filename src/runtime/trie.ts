de27ff7640c0cda380c3cc602c3b397a7c5d5867
Tim Perry <pimterry@gmail.com>
2019-02-12 17:02:24 +0100

    Create trie search functions

--- src/runtime/trie.ts
+++ src/runtime/trie.ts
@@ -1,21 +1,61 @@
-export interface TrieData {
-    ''?: string;
-    [nextKey: string]:
+type TrieValue =
     | TrieData
     | string
     | undefined;
+
+export interface TrieData {
+    ''?: string;
+    [nextKey: string]: TrieValue;
 }
 
 export class Trie {
 
     constructor(private root: TrieData) { }
 
+    private getLongestMatchingPrefix(key: string) {
+        let remainingKey = key;
+        let node: TrieData | undefined = this.root;
+
+        while (node) {
+            const keyLength = Object.keys(node)
+                .reduce((maxLength, key) => Math.max(key.length, maxLength), 0);
+            const keyToMatch = remainingKey.slice(0, keyLength);
+            remainingKey = remainingKey.slice(keyLength);
+
+            const nextNode: TrieValue = node[keyToMatch];
+
+            if (typeof nextNode === 'string') {
+                // We've reached the end of a key - if we're out of
+                // input, that's good, if not it's just a prefix.
+                return {
+                    remainingKey,
+                    matchedKey: key.slice(0, -1 * remainingKey.length),
+                    value: nextNode
+                };
+            } else {
+                node = nextNode;
+            }
+        }
+
+        return undefined;
+    }
+
     /*
      * Given a key, finds an exact match and returns the value.
      * Returns undefined if no match can be found.
      */
     get(key: string): string | undefined {
-        return undefined;
+        const searchResult = this.getLongestMatchingPrefix(key);
+
+        if (!searchResult) return undefined;
+
+        const {
+            remainingKey,
+            value
+        } = searchResult;
+
+        return remainingKey.length === 0 ?
+            value : undefined;
     }
 
     /*
@@ -26,7 +66,13 @@ export class Trie {
      *
      * Returns undefined if no match can be found.
      */
-    getLongestMatchingPrefix(key: string): string | undefined {
-        return undefined;
+    getMatchingPrefix(key: string): string | undefined {
+        const searchResult = this.getLongestMatchingPrefix(key);
+
+        if (!searchResult) return undefined;
+
+        const { value } = searchResult;
+
+        return value;
     }
 }
---
+++ test/runtime/trie.spec.ts
@@ -0,0 +1,110 @@
+import * as _ from "lodash";
+import { expect } from "chai";
+
+import { buildTrie } from "../../src/buildtime/build-index";
+import { Trie } from "../../src/runtime/trie";
+
+describe('Trie searching', () => {
+    describe('with .get()', () => {
+        it('can search a simple trie for a specific value', () => {
+            const trie = new Trie(buildTrie({
+                'a': 'value'
+            }));
+
+            expect(trie.get('a')).to.equal('value');
+        });
+
+        it('returns undefined for missing values', () => {
+            const trie = new Trie(buildTrie({
+                'a': 'value'
+            }));
+
+            expect(trie.get('ab')).to.equal(undefined);
+        });
+
+        it('can search a simple trie for a value thats part way down a path', () => {
+            const trie = new Trie(buildTrie({
+                'a': 'value',
+                'ab': 'value',
+                'abc': 'value2'
+            }));
+
+            expect(trie.get('ab')).to.equal('value');
+        });
+
+        it('can search a large trie quickly', function () {
+            this.timeout(500); // 10000 in < 500ms = < 50 microseconds/search
+            const keys = _.range(10000).map(k => k.toString());
+
+            const trieData = buildTrie(
+                _.zipObject(
+                    keys,
+                    keys.map(k => `value${k}`)
+                )
+            );
+
+            const trie = new Trie(trieData);
+
+            keys.forEach((k) => {
+                expect(trie.get(k)).to.equal(`value${k}`);
+            });
+
+            // This is actually ~3x slower than a bare hashtable, for direct
+            // lookups. As long as it's within an order of magnitude or so though,
+            // that's ok. The real magic comes in prefix searching.
+        });
+    });
+
+    describe('with .getLongestMatchingPrefix()', () => {
+        it('can search a simple trie for an exact value', () => {
+            const trie = new Trie(buildTrie({
+                'a': 'value'
+            }));
+
+            expect(trie.getMatchingPrefix('a')).to.equal('value');
+        });
+
+        it('can search a trie for an exact value part way down a path', () => {
+            const trie = new Trie(buildTrie({
+                'a': 'value',
+                'ab': 'value',
+                'abc': 'value2'
+            }));
+
+            expect(trie.getMatchingPrefix('ab')).to.equal('value');
+        });
+
+        it('can search a trie for a prefix', () => {
+            const trie = new Trie(buildTrie({
+                'a': 'value',
+                'ab': 'value',
+                'abc': 'value2'
+            }));
+
+            expect(trie.getMatchingPrefix('abcd')).to.equal('value2');
+        });
+
+        it('can search a large trie quickly', function () {
+            this.timeout(500); // 10000 in < 500ms = 50 microseconds max
+            const keys = _.range(10000).map(k => k.toString());
+
+            const trieData = buildTrie(
+                _.zipObject(
+                    keys,
+                    keys.map(k => `value${k}`)
+                )
+            );
+
+            const trie = new Trie(trieData);
+
+            keys.forEach((k) => {
+                expect(trie.getMatchingPrefix(k)).to.equal(`value${k}`);
+            });
+
+            // This is the key perf step - doing an equivalent loop on a hashtable
+            // (for all keys, find keys that start with the input, get the longest)
+            // is approx 150x slower than this, for 10000 elements.
+        });
+    });
+
+});
