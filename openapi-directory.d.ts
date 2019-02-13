6219cc7013d8cb74ecaefa5d8b1cb61d86f55f53
Tim Perry <pimterry@gmail.com>
2019-02-13 16:04:06 +0100

    Add type definitions for the lib & api specs

---
+++ openapi-directory.d.ts
@@ -0,0 +1,4 @@
+declare module 'openapi-directory/api/*.json' {
+    import { OpenAPIObject } from 'openapi3-ts';
+    export = OpenAPIObject;
+}
--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,3 +1,7 @@
+// Include a global type declaration that covers all
+// generated api/* files.
+/// <reference path="../../openapi-directory.d.ts" />
+
 import { Trie } from "./trie";
 
 const trieData = require('../../api/_index.json');
